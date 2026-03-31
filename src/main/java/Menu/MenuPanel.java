3221138c038a8efb35b1cc52719a6d8c797a7932
BatlíkFilip <Batlik.f@ssmg.cz>
2026-03-31 11:43:53 +0200

    Projekt pokud nedám tak gg a půjdu na stavbu nebo na skládku.

---
+++ src/main/java/Menu/MenuPanel.java
@@ -0,0 +1,46 @@
+package Menu;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuPanel extends JPanel {
+
+    public MenuPanel(Runnable onStart) {
+
+        setLayout(new BorderLayout());
+        setBackground(Color.gray);
+        JLabel title = new JLabel("Snail", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 72));
+        title.setForeground(Color.WHITE);
+        title.setBorder(BorderFactory.createEmptyBorder(150, 0, 100, 0));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.GRAY);
+
+        JButton startButton = new JButton("Start Game");
+        startButton.setFont(new Font("Arial", Font.BOLD, 32));
+        startButton.setFocusPainted(false);
+        startButton.setBackground(Color.WHITE);
+        startButton.setForeground(Color.BLACK);
+        startButton.setPreferredSize(new Dimension(250, 80));
+
+
+
+
+
+
+        startButton.addActionListener(e -> {
+            if (onStart != null) {
+                onStart.run();
+            }
+        });
+
+        buttonPanel.add(startButton);
+
+        // ===== Add to layout =====
+        add(title, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
+
+
+    }
+}
+
